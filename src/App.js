c8e5a2b6633385335a0f1010af5c4eb76baf50b1
AleksandrKompanetc <moneymacker7@gmail.com>
2024-12-27 23:20:05 +0200

    adding component Result

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+function Result() {
+  return (
+    <div className='result'>
+      <h2>You guessed three answers out of ten</h2>
+      <button>Try again</button>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <Result />
     </div>
   );
 }
